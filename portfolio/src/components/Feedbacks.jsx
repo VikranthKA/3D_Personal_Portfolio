016a48ea70efb26602b8acad30cc08546432e854
vikranth <vikranthka@gmail.com>
2024-07-07 12:30:00 +0530

    creating the testimonial

--- portfolio/src/components/Feedbacks.jsx
+++ portfolio/src/components/Feedbacks.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import {motion} from 'framer-motion'
+import {styles} from "../style"
+import {SectionWrapper} from "../hoc"
+import {fadeIn,textVariant} from "../utils/motion"
+import {testimonials} from "../constants"
 
+const FeedbackCard =()=>{
+  
+}
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded[20px]'>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p>What others say</p>
+          <h2>Testomonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX}-mt-20 pb-15 flex flex-wrap pag-7`}>
+        {testimonials.map((testimonial,index)=>(
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+
+
+      </div>
+    </div>
   )
 }
 
