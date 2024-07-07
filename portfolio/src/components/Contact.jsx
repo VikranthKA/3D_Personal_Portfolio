ee0d418e7abc47b64f310443a36f8a51940312dd
vikranth <vikranthka@gmail.com>
2024-07-07 11:20:51 +0530

    complted tech working on the Works/experience

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,11 @@
+// src/components/Contact.jsx
+const Contact = () => {
+  return (
+    <div className="bg-white p-8 rounded-md shadow-lg">
+      <h2 className="text-2xl font-bold text-gray-900">Contact Us</h2>
+      <p className="text-gray-600">Feel free to reach out to us at any time.</p>
+    </div>
+  );
+};
+
+export default Contact;
