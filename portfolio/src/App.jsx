deaada63178bec6a32418dd4aedd4faf44e8eff8
vikranth <vikranthka@gmail.com>
2024-07-10 07:54:53 +0530

    edited the contactd

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -24,9 +24,8 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
-          {/* <Contact />
-           */}
-           <EarthCanvas/>
+          <Contact />
+          
         </div>
 
 
