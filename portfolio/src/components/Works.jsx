96c8f05a9cd0b604df2b19815f5898707e292da2
vikranth <vikranthka@gmail.com>
2025-02-05 20:21:30 +0530

    added slice to desc

--- portfolio/src/components/Works.jsx
+++ portfolio/src/components/Works.jsx
@@ -52,7 +52,7 @@ const ProjectCard = ({
     <div className='mt-5 flex justify-between items-center'>
       <div>
         <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-        <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        <p className='mt-2 text-secondary text-[14px]'>{description.slice(0,120)}</p>
       </div>
       {live && (
         <a
